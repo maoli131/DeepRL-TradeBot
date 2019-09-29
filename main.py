3045b17c1d55e3ff352ce62fdacf47f38066ac10
Vincent-Li-9701 <vincent.li.9701@gmail.com>
2019-09-29 12:22:23 -0700

    fixing the bugs in trading environment

---
+++ main.py
@@ -0,0 +1,30 @@
+import gym
+import json
+import datetime as dt
+
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import PPO2
+
+from env.TradingEnv import TradingEnv
+
+import pandas as pd
+
+#read the historical stock data
+name = input("Please enter the stock historical data file name: ") 
+
+df = pd.read_csv('./data/invest_data/' + name, index_col=0)
+df = df.iloc[::-1].reset_index(drop=True)
+df = df.sort_values('Date')
+
+# The algorithms require a vectorized environment to run
+env = DummyVecEnv([lambda: TradingEnv(df)])
+
+model = PPO2(MlpPolicy, env, verbose=1)
+model.learn(total_timesteps=20000)
+
+obs = env.reset()
+for i in range(2000):
+    action, _states = model.predict(obs)
+    obs, rewards, done, info = env.step(action)
+    env.render(title=name[:-13])
